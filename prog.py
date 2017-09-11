ece8c05756887dc4dc3cdb9ca96374f1d43a9dba
morata1009 <31612314+morata1009@users.noreply.github.com>
2017-09-11 12:29:57 +0530

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+g=(int(input("Enter a number")))
+if (g>0):
+    print(g, "Is a positive number")
+elif(g==0):
+    print("The number is zero")
+elif(g<0):
+    print(g, "Is a negative number")
